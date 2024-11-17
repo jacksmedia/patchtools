31cd16da55443d1f465675dc4f382517ad42c895
jacksmedia <alex@j4cks.com>
2024-11-17 13:49:34 -0800

    🛠 json intake script

---
+++ json-intake.py
@@ -0,0 +1,12 @@
+import json
+
+# for use with a file called ./bytes.json 
+# prints object data to console
+with open('bytes.json', 'r') as f:
+    data = json.load(f)
+
+for character, info in data.items():
+    print(f"Character: {character}")
+    print(f"  Sprites Start: {info['sprites_start']}")
+    print(f"  Palette: {info['palette']}")
+    print(f"  Palette Start: {info['pal_start']}")
