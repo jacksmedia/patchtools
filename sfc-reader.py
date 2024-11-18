c42ee3f5bd907ed20576315bc68ff06584ba3957
jacksmedia <alex@j4cks.com>
2024-11-17 17:19:09 -0800

    📈 adds map sprites data to tool

--- sfc-reader.py
+++ sfc-reader.py
@@ -13,15 +13,22 @@ with open('offset_data.json', 'r') as f:
 rom_filename = '_original-rom.sfc'
 
 for character, info in data.items():
-    sprites_start = int(info['sprites_start'][1:], 16) # 16 bc hex number
-    pal_start = int(info['pal_start'][1:], 16)
+    battle_sprites_start = int(info['battle_sprites_start'][1:], 16) # 16 bc hex number
+    pal_start = int(info['pal_start'][1:], 16) # [1:] strips the $
+    map_sprites_start = int(info['map_sprites_start'][1:], 16) # [1:] strips the $
 
-    # Read 16 bytes of sprite data
-    sprite_data = read_bytes(rom_filename, sprites_start, 1536) # 48 tiles of 32 bytes each
+
+    # Read tiles of sprite data
+    # will use the line below for map sprites
+    battle_sprite_data = read_bytes(rom_filename, battle_sprites_start, 2048) # 48 tiles of 32 bytes each
 
     # Read 32 bytes of palette data
     palette_data = read_bytes(rom_filename, pal_start, 32) # 16 colors of 2 bytes each
 
+    # Read 1536 bytes of map sprite data
+    map_sprite_data = read_bytes(rom_filename, map_sprites_start, 1536) # 48 tiles of 32 bytes each
+
     print(f"Character: {character}")
-    print(f"  Sprite Data: {sprite_data.hex()}")
+    print(f"  Sprite Data: {battle_sprite_data.hex()}")
     print(f"  Palette Data: {palette_data.hex()}")
+    print(f"  Map Sprite Data: {map_sprite_data.hex()}")
